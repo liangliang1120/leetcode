54d22759f52dc70d9f2434ece58ac391353f3712
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-06-04 22:31:43 -0700

    Update 90-Subsets-II.py

--- solutions/90-Subsets-II.py
+++ solutions/90-Subsets-II.py
@@ -1,5 +1,23 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        subset = []
+        res = []
+        nums.sort()
+        def backtrack(start):
+            res.append(subset.copy())
+            for i in range(start, n):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                subset.append(nums[i])
+                backtrack(i + 1)
+                subset.pop()
+        backtrack(0)
+        return res
+    
+    
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
         subset = []
         n = len(nums)
