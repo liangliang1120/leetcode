d677a642a6ceba097be4abc1e5d74021768d6ac3
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-06-05 09:02:19 -0700

    Update 46-Permutations.py

--- solutions/46-Permutations.py
+++ solutions/46-Permutations.py
@@ -3,6 +3,29 @@ class Solution:
         n = len(nums)
         subset = []
         res = []
+        visited = [False] * n
+        nums.sort()
+        def backtrack():
+            if len(subset) == n:
+                res.append(subset.copy())
+                return
+            for i in range(n):
+                if not visited[i]:
+                    visited[i] = True
+                    subset.append(nums[i])
+                    backtrack()
+                    subset.pop()
+                    visited[i] = False
+
+           
+        backtrack()
+        return res
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        subset = []
+        res = []
         def backtrack(i, visited):
             if i >= n:
                 res.append(subset.copy())
