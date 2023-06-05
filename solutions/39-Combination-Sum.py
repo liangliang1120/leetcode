e371e89ef0b9b1586b35223b32ce8270e4627fc3
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-06-04 21:22:51 -0700

    Update 39-Combination-Sum.py

--- solutions/39-Combination-Sum.py
+++ solutions/39-Combination-Sum.py
@@ -3,6 +3,29 @@ class Solution:
         subset = []
         res = []
         n = len(candidates)
+        candidates.sort()
+        def backtrack(begin):
+            if sum(subset) > target:
+                return
+            if sum(subset) == target:
+                res.append(subset.copy())
+                return
+            for index in range(begin, n):
+                if sum(subset) > target:
+                    break
+                
+                subset.append(candidates[index])
+                backtrack(index)
+                subset.pop()
+
+        backtrack(0)
+        return res
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        subset = []
+        res = []
+        n = len(candidates)
         def backtrack(i):
             
             if sum(subset) == target:
