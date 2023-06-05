71b1bee94a1b4bb44595f5ab5be0ae0abdb46e94
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-06-04 22:15:29 -0700

    Update 78Subsets.py

--- solutions/78Subsets.py
+++ solutions/78Subsets.py
@@ -3,6 +3,22 @@ class Solution:
         res = []
         n = len(nums)
         subset = []
+        def backtrack(start):
+            res.append(subset.copy())
+            for i in range(start, n):
+                subset.append(nums[i])
+                backtrack(i + 1)
+                subset.pop()
+
+        backtrack(0)
+        return res
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        subset = []
         def backtrack(i):
             if i >= n:
                 res.append(subset.copy())
