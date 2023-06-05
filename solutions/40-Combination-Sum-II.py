8e3abba90d0012d9e94f7e2e38269b64f40fa5dc
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-06-04 21:46:03 -0700

    Update 40-Combination-Sum-II.py

--- solutions/40-Combination-Sum-II.py
+++ solutions/40-Combination-Sum-II.py
@@ -1,44 +1,23 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        n = len(candidates)
-        res = []
         subset = []
+        res = []
+        n = len(candidates)
         candidates.sort()
-        def backtrack(i):
-            if sum(subset) == target and subset not in res:
-                res.append(subset.copy())
-            if i >= n or sum(subset) > target:
+        def backtrack(begin):
+            if sum(subset) > target:
                 return
-            # 1
-            subset.append(candidates[i])
-            backtrack(i + 1)
-            subset.pop()
-            # 2
-            while i < n - 1 and candidates[i] == candidates[i+1]:
-                i += 1
-            backtrack(i + 1)
-        backtrack(0)
-        return res
-
-    
-class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        n = len(candidates)
-        res = []
-        subset = []
-        candidates.sort()
-        def backtrack(i):
             if sum(subset) == target:
                 res.append(subset.copy())
                 return
-
-            for x in range(i, n):
-                if candidates[x] + sum(subset) > target:
+            for index in range(begin, n):
+                if sum(subset) + candidates[index] > target:
                     break
-                if x > i and candidates[x - 1] == candidates[x]:
+                if index > begin and candidates[index] == candidates[index - 1]:
                     continue
-                subset.append(candidates[x])
-                backtrack(x + 1)
+                
+                subset.append(candidates[index])
+                backtrack(index + 1)
                 subset.pop()
 
         backtrack(0)
