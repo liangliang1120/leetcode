32ec3c14cd14a8a82b039642f5dc4f8b180b8d96
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-06-07 10:32:00 -0700

    Create 215-Kth-Largest-Element-in-an-Array.py

---
+++ solutions/215-Kth-Largest-Element-in-an-Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        while len(nums) > k:
+            heapq.heappop(nums)
+        reverse_num = [-x for x in nums]
+        return  -reverse_num[0]
