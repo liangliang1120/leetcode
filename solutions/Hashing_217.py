b21eaf8bd26b312dfbf6654a446b73d50c387eea
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-07-11 18:28:37 -0700

    Update Hashing_217.py

--- solutions/Hashing_217.py
+++ solutions/Hashing_217.py
@@ -6,3 +6,36 @@ class Solution:
                 return True
             hashset.add(n)
         return False
+
+
+
+# Java sort
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+# Java hash
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int x : nums) {
+            if (set.contains(x)) {
+                return true;
+            } else {
+                set.add(x);
+            }
+        }
+        return false;
+    }
+}
