d733a44b1dffb50030d02a49be7fd3404eba5a5b
Lily1120 <35073431+liangliang1120@users.noreply.github.com>
2023-07-12 19:21:16 -0700

    Update 0242-Valid-Anagram.py

--- solutions/0242-Valid-Anagram.py
+++ solutions/0242-Valid-Anagram.py
@@ -12,6 +12,22 @@ class Solution:
             thash[tn] += 1
         return thash == shash
 
+# java sort
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
+    }
+}
+
 
 # java
 class Solution {
